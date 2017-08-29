1fce61be9b84f1e6a7fa8ec2d09f2a59c52dd190
Gerben <gerbenvaneerten@gmail.com>
2017-08-29 12:21:40 +0200

    Install from git

--- install/ubuntu16.04.sh
+++ install/ubuntu16.04.sh
@@ -12,55 +12,13 @@ apt-get -y install php7.0
 apt-get -y install apache2
 apt-get -y install php libapache2-mod-php
 
-svn co http://svn.openstreetmap.org/applications/routing/yours/trunk/www yours
-cp -a yours /var/www/yours
+cp -a src/www /var/www/yours
 
 ## Configure apache2
-echo "<VirtualHost *:80>
-	# The ServerName directive sets the request scheme, hostname and port that
-	# the server uses to identify itself. This is used when creating
-	# redirection URLs. In the context of virtual hosts, the ServerName
-	# specifies what hostname must appear in the request's Host: header to
-	# match this virtual host. For the default virtual host (this file) this
-	# value is not decisive as it is used as a last resort host regardless.
-	# However, you must set it for any further virtual host explicitly.
-	#ServerName www.example.com
-
-	ServerAdmin webmaster@localhost
-	DocumentRoot /var/www/yours
-
-
-	# Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
-	# error, crit, alert, emerg.
-	# It is also possible to configure the loglevel for particular
-	# modules, e.g.
-	#LogLevel info ssl:warn
-
-	ErrorLog ${APACHE_LOG_DIR}/error.log
-	CustomLog ${APACHE_LOG_DIR}/access.log combined
-
-	<Directory />
-		Order allow,deny
-		Allow from all
-	</Directory>
-	# For most configuration files from conf-available/, which are
-	# enabled or disabled at a global level, it is possible to
-	# include a line for only one particular virtual host. For example the
-	# following line enables the CGI configuration for this host only
-	# after it has been globally disabled with "a2disconf".
-	#Include conf-available/serve-cgi-bin.conf
-</VirtualHost>
-" > /etc/apache2/sites-available/001-yours.conf
+cp src/debianfs/etc/apache2/sites-available/001-yours.conf /etc/apache2/sites-available/
 ln -s /etc/apache2/sites-available/001-yours.conf /etc/apache2/sites-enabled/001-yours.conf
-
 rm /etc/apache2/sites-enabled/000-default.conf 
 apache2ctl restart
-
-
-
-
-
-
 
 ###########################
 ###########################
@@ -123,8 +81,10 @@ cd
 ###########################
 ###########################
 #Fix hardcode paths in php files
-sed -i 's:^$www_dir.*:$www_dir = '/var/www/yours/';:g' /var/www/yours/gosmore.php
-sed -i 's:^$yours_dir.*:$yours_dir = '/var/www/gosmore/';:g' /var/www/yours/gosmore.php
+sed -i 's:^$www_dir.*:$www_dir = '/var/www/yours/';:g' /var/www/yours/api/dev/settings.php
+sed -i 's:^$yours_dir.*:$yours_dir = '/var/www/gosmore/';:g' /var/www/yours/api/dev/gosmore.php
+sed -i 's:^$www_dir.*:$www_dir = '/var/www/yours/';:g' /var/www/yours/api/1.0/settings.php
+sed -i 's:^$yours_dir.*:$yours_dir = '/var/www/gosmore/';:g' /var/www/yours/api/1.0/gosmore.php
 
 #Web app is calling api/<version>/route.php which does not exist
 ln -s /var/www/yours/api/dev/gosmore.php /var/www/yours/api/dev/route.php
